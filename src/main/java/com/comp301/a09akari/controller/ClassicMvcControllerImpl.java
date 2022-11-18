49edf0317b17add3e5a8d3cc9920653de902f8d3
Heri Ongechi <hongechi@unc.edu>
2022-11-18 12:20:21 -0500

    Submission

--- src/main/java/com/comp301/a09akari/controller/ClassicMvcControllerImpl.java
+++ src/main/java/com/comp301/a09akari/controller/ClassicMvcControllerImpl.java
@@ -2,28 +2,31 @@ package com.comp301.a09akari.controller;
 
 import com.comp301.a09akari.model.Model;
 
-public class ClassicMvcControllerImpl implements ClassicMvcController{
+public class ClassicMvcControllerImpl implements ClassicMvcController {
 
-    private Model model;
-    public ClassicMvcControllerImpl(Model model){
-        this.model = model;
-    }
-    public void clickNextPuzzle(){
-        model.setActivePuzzleIndex(model.getActivePuzzleIndex() +1);
-        model.getActivePuzzle();
+  private Model model;
 
-    }
-    public void clickPrevPuzzle(){
-        model.setActivePuzzleIndex(model.getActivePuzzleIndex()-1);
-        model.getActivePuzzle();
-    }
-    public void clickRandPuzzle(){
+  public ClassicMvcControllerImpl(Model model) {
+    this.model = model;
+  }
 
-    }
-    public void clickResetPuzzle(){
-        model.resetPuzzle();
-    }
-    public void clickCell(int r, int c){
-        model.addLamp(r,c);
-    }
+  public void clickNextPuzzle() {
+    model.setActivePuzzleIndex(model.getActivePuzzleIndex() + 1);
+    model.getActivePuzzle();
+  }
+
+  public void clickPrevPuzzle() {
+    model.setActivePuzzleIndex(model.getActivePuzzleIndex() - 1);
+    model.getActivePuzzle();
+  }
+
+  public void clickRandPuzzle() {}
+
+  public void clickResetPuzzle() {
+    model.resetPuzzle();
+  }
+
+  public void clickCell(int r, int c) {
+    model.addLamp(r, c);
+  }
 }
